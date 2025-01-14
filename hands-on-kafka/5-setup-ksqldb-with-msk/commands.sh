2c49a9bb2b359dc0d28ff5f579ba91336d67a25e
Atin Gupta <atingupta@email.com>
2025-01-14 13:33:02 +0530

    -

--- hands-on-kafka/5-setup-ksqldb-with-msk/commands.sh
+++ hands-on-kafka/5-setup-ksqldb-with-msk/commands.sh
@@ -1,3 +1,13 @@
 nano .env
 nano ksql-config/ksql-server.properties
 docker-compose up -d
+
+   mkdir -p ~/kafka/libs
+   curl -L -o ~/kafka/libs/aws-msk-iam-auth.jar https://github.com/aws/aws-msk-iam-auth/releases/download/v2.2.0/aws-msk-iam-auth-2.2.0-all.jar
+   jar tf ~/kafka/libs/aws-msk-iam-auth.jar | grep IAMClientCallbackHandler
+
+   echo 'export CLASSPATH=$CLASSPATH:/home/$USER/kafka/libs/aws-msk-iam-auth.jar' >> ~/.bashrc
+
+   source ~/.bashrc
+
+   
