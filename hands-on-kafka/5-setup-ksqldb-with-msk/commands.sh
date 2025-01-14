cc688ed4a4cd2a66c561b30ddd96b5245f6c73ee
Atin Gupta <atingupta@email.com>
2025-01-14 16:08:58 +0530

    -

--- hands-on-kafka/5-setup-ksqldb-with-msk/commands.sh
+++ hands-on-kafka/5-setup-ksqldb-with-msk/commands.sh
@@ -1,18 +1,19 @@
+aws kafka list-clusters
 sudo yum update
 sudo yum install git -y
 git clone https://github.com/atingupta2005/kafka-on-aws-Jan-25
 cd ~/kafka-on-aws-Jan-25/hands-on-kafka/5-setup-ksqldb-with-msk
-
 nano .env
 nano ksql-config/ksql-server.properties
-docker-compose up -d
+sudo yum install -y docker
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+sudo docker-compose up -d
 
-   mkdir -p ~/kafka/libs
-   curl -L -o ~/kafka/libs/aws-msk-iam-auth.jar https://github.com/aws/aws-msk-iam-auth/releases/download/v2.2.0/aws-msk-iam-auth-2.2.0-all.jar
-   jar tf ~/kafka/libs/aws-msk-iam-auth.jar | grep IAMClientCallbackHandler
 
-   echo 'export CLASSPATH=$CLASSPATH:/home/$USER/kafka/libs/aws-msk-iam-auth.jar' >> ~/.bashrc
+docker exec -it ksqldb-cli ksql http://ksqldb-server:8088
 
-   source ~/.bashrc
-
-   
--- hands-on-kafka/5-setup-ksqldb-with-msk/entrypoint.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-tail -f /dev/null  # Keeps the container running for debugging
