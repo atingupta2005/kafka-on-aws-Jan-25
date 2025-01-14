4ac65029cbeccef89276b06ad9f6300d1cdf8f41
Atin Gupta <atingupta@email.com>
2025-01-14 14:11:14 +0530

    -

--- hands-on-kafka/5-setup-ksqldb-with-msk/entrypoint.sh
+++ hands-on-kafka/5-setup-ksqldb-with-msk/entrypoint.sh
@@ -5,5 +5,4 @@ if [ "$DEBUG_MODE" = "true" ]; then
   tail -f /dev/null  # Keeps the container running for debugging
 else
   echo "Starting ksqlDB Server..."
-  exec /etc/confluent/docker/ksqldb-server  # Correct entrypoint for ksqlDB
 fi
