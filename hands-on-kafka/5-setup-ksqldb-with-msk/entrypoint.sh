ac62aca2fed2cf86d323d53c6b515eb734cb093f
Atin Gupta <atingupta@email.com>
2025-01-14 14:13:50 +0530

    -

--- hands-on-kafka/5-setup-ksqldb-with-msk/entrypoint.sh
+++ hands-on-kafka/5-setup-ksqldb-with-msk/entrypoint.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-if [ "$DEBUG_MODE" = "true" ]; then
-  echo "Debug mode enabled. Starting a bash shell..."
-  tail -f /dev/null  # Keeps the container running for debugging
-else
-  echo "Starting ksqlDB Server..."
-fi
+tail -f /dev/null  # Keeps the container running for debugging
